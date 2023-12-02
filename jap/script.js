dff83fba14bf3d723b4a30ffd836c86f500037cd
Vito Tang <vito.tang@tpgtelecom.com.au>
2023-12-02 17:25:27 +1100

    add folders

---
+++ jap/script.js
@@ -0,0 +1,670 @@
+
+var gojuons = [
+  {
+    'hiragana': 'あ',
+    'katakana': 'ア',
+    'roman': 'a',
+  },
+  {
+    'hiragana': 'い',
+    'katakana': 'イ',
+    'roman': 'i',
+  },
+  {
+    'hiragana': 'う',
+    'katakana': 'ウ',
+    'roman': 'u',
+  },
+  {
+    'hiragana': 'え',
+    'katakana': 'エ',
+    'roman': 'e',
+  },
+  {
+    'hiragana': 'お',
+    'katakana': 'オ',
+    'roman': 'o',
+  },
+  {
+    'hiragana': 'か',
+    'katakana': 'カ',
+    'roman': 'ka',
+  },
+  {
+    'hiragana': 'き',
+    'katakana': 'キ',
+    'roman': 'ki',
+  },
+  {
+    'hiragana': 'く',
+    'katakana': 'ク',
+    'roman': 'ku',
+  },
+  {
+    'hiragana': 'け',
+    'katakana': 'ケ',
+    'roman': 'ke',
+  },
+  {
+    'hiragana': 'こ',
+    'katakana': 'コ',
+    'roman': 'ko',
+  },
+  {
+    'hiragana': 'さ',
+    'katakana': 'サ',
+    'roman': 'sa',
+  },
+  {
+    'hiragana': 'し',
+    'katakana': 'シ',
+    'roman': 'shi',
+  },
+  {
+    'hiragana': 'す',
+    'katakana': 'ス',
+    'roman': 'su',
+  },
+  {
+    'hiragana': 'せ',
+    'katakana': 'セ',
+    'roman': 'se',
+  },
+  {
+    'hiragana': 'そ',
+    'katakana': 'ソ',
+    'roman': 'so',
+  },
+  {
+    'hiragana': 'た',
+    'katakana': 'タ',
+    'roman': 'ta',
+  },
+  {
+    'hiragana': 'ち',
+    'katakana': 'チ',
+    'roman': 'chi',
+  },
+  {
+    'hiragana': 'つ',
+    'katakana': 'ツ',
+    'roman': 'tsu',
+  },
+  {
+    'hiragana': 'て',
+    'katakana': 'テ',
+    'roman': 'te',
+  },
+  {
+    'hiragana': 'と',
+    'katakana': 'ト',
+    'roman': 'to',
+  },
+  {
+    'hiragana': 'な',
+    'katakana': 'ナ',
+    'roman': 'na',
+  },
+  {
+    'hiragana': 'に',
+    'katakana': 'ニ',
+    'roman': 'ni',
+  },
+  {
+    'hiragana': 'ぬ',
+    'katakana': 'ヌ',
+    'roman': 'nu',
+  },
+  {
+    'hiragana': 'ね',
+    'katakana': 'ネ',
+    'roman': 'ne',
+  },
+  {
+    'hiragana': 'の',
+    'katakana': 'ノ',
+    'roman': 'no',
+  },
+  {
+    'hiragana': 'は',
+    'katakana': 'ハ',
+    'roman': 'ha',
+  },
+  {
+    'hiragana': 'ひ',
+    'katakana': 'ヒ',
+    'roman': 'hi',
+  },
+  {
+    'hiragana': 'ふ',
+    'katakana': 'フ',
+    'roman': 'fu',
+  },
+  {
+    'hiragana': 'へ',
+    'katakana': 'ヘ',
+    'roman': 'he',
+  },
+  {
+    'hiragana': 'ほ',
+    'katakana': 'ホ',
+    'roman': 'ho',
+  },
+  {
+    'hiragana': 'ま',
+    'katakana': 'マ',
+    'roman': 'ma',
+  },
+  {
+    'hiragana': 'み',
+    'katakana': 'ミ',
+    'roman': 'mi',
+  },
+  {
+    'hiragana': 'む',
+    'katakana': 'ム',
+    'roman': 'mu',
+  },
+  {
+    'hiragana': 'め',
+    'katakana': 'メ',
+    'roman': 'me',
+  },
+  {
+    'hiragana': 'も',
+    'katakana': 'モ',
+    'roman': 'mo',
+  },
+  {
+    'hiragana': 'や',
+    'katakana': 'ヤ',
+    'roman': 'ya',
+  },
+  {
+    'hiragana': 'ゆ',
+    'katakana': 'ユ',
+    'roman': 'yu',
+  },
+  {
+    'hiragana': 'よ',
+    'katakana': 'ヨ',
+    'roman': 'yo',
+  },
+  {
+    'hiragana': 'ら',
+    'katakana': 'ラ',
+    'roman': 'ra',
+  },
+  {
+    'hiragana': 'り',
+    'katakana': 'リ',
+    'roman': 'ri',
+  },
+  {
+    'hiragana': 'る',
+    'katakana': 'ル',
+    'roman': 'ru',
+  },
+  {
+    'hiragana': 'れ',
+    'katakana': 'レ',
+    'roman': 're',
+  },
+  {
+    'hiragana': 'ろ',
+    'katakana': 'ロ',
+    'roman': 'ro',
+  },
+  {
+    'hiragana': 'わ',
+    'katakana': 'ワ',
+    'roman': 'wa',
+  },
+  {
+    'hiragana': 'を',
+    'katakana': 'ヲ',
+    'roman': 'wo',
+  },
+  {
+    'hiragana': 'ん',
+    'katakana': 'ン',
+    'roman': 'n',
+  }
+];
+var words = [
+  {
+    'word': 'はい',
+    'roman': 'hai',
+    'meaning': 'Yes'
+  },
+  {
+    'word': 'いいえ',
+    'roman': 'iie',
+    'meaning': 'No'
+  },
+  {
+    'word': '私',
+    'roman': 'watashi',
+    'meaning': 'I'
+  },
+  {
+    'word': 'あなた',
+    'roman': 'anata',
+    'meaning': 'You'
+  },
+  {
+    'word': 'これ',
+    'roman': 'kore',
+    'meaning': 'This'
+  },
+  {
+    'word': 'それ',
+    'roman': 'sore',
+    'meaning': 'That'
+  },
+  {
+    'word': '彼',
+    'roman': 'kare',
+    'meaning': 'He'
+  },
+  {
+    'word': '彼女',
+    'roman': 'kanojo',
+    'meaning': 'She'
+  },
+  {
+    'word': '彼ら',
+    'roman': 'karera',
+    'meaning': 'They'
+  },
+  {
+    'word': 'いち',
+    'roman': 'ichi',
+    'meaning': 'One'
+  },
+  {
+    'word': 'に',
+    'roman': 'ni',
+    'meaning': 'Two'
+  },
+  {
+    'word': 'さん',
+    'roman': 'san',
+    'meaning': 'Three'
+  },
+  {
+    'word': 'し or よん',
+    'roman': 'shi or yon',
+    'meaning': 'Four'
+  },
+  {
+    'word': 'ご',
+    'roman': 'go',
+    'meaning': 'Five'
+  },
+  {
+    'word': 'ろく',
+    'roman': 'roku',
+    'meaning': 'Six'
+  },
+  {
+    'word': 'なな or しち',
+    'roman': 'nana or shichi',
+    'meaning': 'Seven'
+  },
+  {
+    'word': 'はち',
+    'roman': 'hachi',
+    'meaning': 'Eight'
+  },
+  {
+    'word': 'く or きゅう',
+    'roman': 'ku or kyuu',
+    'meaning': 'Nine'
+  },
+  {
+    'word': 'じゅう',
+    'roman': 'juu',
+    'meaning': 'Ten'
+  },
+  {
+    'word': '今日',
+    'roman': 'kyou',
+    'meaning': 'Today'
+  },
+  {
+    'word': '明日',
+    'roman': 'ashita',
+    'meaning': 'Tomorrow'
+  },
+  {
+    'word': '昨日',
+    'roman': 'kinou',
+    'meaning': 'Yesterday'
+  },
+  {
+    'word': '今',
+    'roman': 'ima',
+    'meaning': 'Now'
+  },
+  {
+    'word': '前に',
+    'roman': 'mae ni',
+    'meaning': 'Before'
+  },
+  {
+    'word': '後で',
+    'roman': 'ato de',
+    'meaning': 'Later'
+  },
+  {
+    'word': '家',
+    'roman': 'ie or uchi',
+    'meaning': 'Home'
+  },
+  {
+    'word': '店',
+    'roman': 'mise',
+    'meaning': 'Shop'
+  },
+  {
+    'word': '映画',
+    'roman': 'eiga',
+    'meaning': 'Movie'
+  },
+  {
+    'word': '学校',
+    'roman': 'gakkou',
+    'meaning': 'School'
+  },
+  {
+    'word': '車',
+    'roman': 'kuruma',
+    'meaning': 'Car'
+  },
+  {
+    'word': '町',
+    'roman': 'machi',
+    'meaning': 'Town'
+  },
+  {
+    'word': '音楽',
+    'roman': 'ongaku',
+    'meaning': 'Music'
+  },
+  {
+    'word': '家族',
+    'roman': 'kazoku',
+    'meaning': 'Family'
+  },
+  {
+    'word': '出身',
+    'roman': 'shusshin',
+    'meaning': 'Hometown'
+  },
+  {
+    'word': 'トイレ',
+    'roman': 'toire',
+    'meaning': 'Bathroom'
+  },
+  {
+    'word': 'する',
+    'roman': 'suru',
+    'meaning': 'To do'
+  },
+  {
+    'word': 'です',
+    'roman': 'desu',
+    'meaning': 'To be'
+  },
+  {
+    'word': 'なる',
+    'roman': 'naru',
+    'meaning': 'To become'
+  },
+  {
+    'word': 'ある',
+    'roman': 'aru',
+    'meaning': 'There is (inanimate)'
+  },
+  {
+    'word': 'いる',
+    'roman': 'iru',
+    'meaning': 'There is (living)'
+  },
+  {
+    'word': '行く',
+    'roman': 'iku',
+    'meaning': 'To go'
+  },
+  {
+    'word': '言う',
+    'roman': 'iu',
+    'meaning': 'To say'
+  },
+  {
+    'word': '見る',
+    'roman': 'miru',
+    'meaning': 'To see'
+  },
+  {
+    'word': '来る',
+    'roman': 'kuru',
+    'meaning': 'To come'
+  },
+  {
+    'word': '食べる',
+    'roman': 'taberu',
+    'meaning': 'To eat'
+  }
+];
+var sentences = [
+  {
+    'sentences': 'こんにちは',
+    'roman': 'Konnichiwa',
+    'meaning': 'Hello'
+  },
+  {
+    'sentences': 'おはようございます',
+    'roman': 'Ohayou gozaimasu',
+    'meaning': 'Good morning'
+  },
+  {
+    'sentences': 'こんばんは',
+    'roman': 'Konbanwa',
+    'meaning': 'Good evening'
+  },
+  {
+    'sentences': 'お元気ですか',
+    'roman': 'Ogenki desu ka?',
+    'meaning': 'How are you?'
+  },
+  {
+    'sentences': '元気です',
+    'roman': 'Genki desu',
+    'meaning': 'I’m good/I’ve been doing well, thanks'
+  },
+  {
+    'sentences': 'お久しぶりですね',
+    'roman': 'Ohisashiburi desu ne ',
+    'meaning': 'Long time no see'
+  },
+  {
+    'sentences': '名前は何ですか',
+    'roman': 'Namae wa nan desu ka?',
+    'meaning': 'What’s your name?'
+  },
+  {
+    'sentences': '私の名前は＿＿です',
+    'roman': 'Watashi no namae wa _____ desu',
+    'meaning': 'My name is ______'
+  },
+  {
+    'sentences': 'どこから来ましたか',
+    'roman': 'Doko kara kimashita ka?',
+    'meaning': 'Where are you from?'
+  },
+  {
+    'sentences': '私は＿＿から来ました',
+    'roman': 'Watashi wa ______ kara kimashita',
+    'meaning': 'I’m from __________.'
+  },
+  {
+    'sentences': 'そうですか',
+    'roman': 'Sou desu ka?',
+    'meaning': 'Is that so?/Really?/I see'
+  },
+  {
+    'sentences': 'ありがとうございます',
+    'roman': 'Arigatou gozaimasu',
+    'meaning': 'Thank you'
+  },
+  {
+    'sentences': 'どういたしまして',
+    'roman': 'Douitashimashite',
+    'meaning': 'You’re welcome'
+  },
+  {
+    'sentences': 'すみません',
+    'roman': 'Sumimasen',
+    'meaning': 'I’m sorry/excuse me'
+  },
+  {
+    'sentences': 'ごめんなさい',
+    'roman': 'Gomen nasai',
+    'meaning': 'I’m sorry'
+  },
+  {
+    'sentences': 'よろしくおねがいします',
+    'roman': 'Yoroshiku onegaishimasu',
+    'meaning': 'I’m in your debt!'
+  },
+  {
+    'sentences': 'いただきます',
+    'roman': 'Itadakimasu',
+    'meaning': 'Let’s dig in'
+  },
+  {
+    'sentences': 'ごちそうさまでした',
+    'roman': 'Gochisousama deshita',
+    'meaning': 'That was delicious'
+  },
+  {
+    'sentences': 'いいですよ',
+    'roman': 'ii desu yo',
+    'meaning': 'It’s good'
+  },
+  {
+    'sentences': 'だめです',
+    'roman': 'dame desu',
+    'meaning': 'It’s bad'
+  },
+  {
+    'sentences': 'もう一度お願いします',
+    'roman': 'mou ichido onegai shimasu',
+    'meaning': 'Again, please.'
+  },
+  {
+    'sentences': 'ゆっくりお願いします',
+    'roman': 'yukkuri onegai shimasu',
+    'meaning': 'More slowly, please'
+  },
+  {
+    'sentences': 'わかりません',
+    'roman': 'wakarimasen',
+    'meaning': 'I don’t understand'
+  },
+  {
+    'sentences': '良かった',
+    'roman': 'yokatta',
+    'meaning': 'Great or I’m glad'
+  },
+  {
+    'sentences': '少し日本語を話します',
+    'roman': 'Sukoshi nihongo wo hanashimasu',
+    'meaning': 'I speak a little Japanese'
+  },
+  {
+    'sentences': 'また会いましょう',
+    'roman': 'Mata aimashou',
+    'meaning': 'Let’s meet again'
+  }
+];
+var jpFn = {
+  init: function(){
+      jpFn.assignClick();
+      jpFn.randomPick();
+    
+  },
+  selectedType: '',
+  selectedCat: '',
+  unselectedCat: [],
+  previousType: '',
+  randomPick: function(){
+    
+    var item = '';
+    var front = '';
+    var back1 = '';
+    var back2 = '';
+    $('.flip').removeClass(jpFn.previousType);
+
+    switch (jpFn.selectedType){
+      case 'word':
+        item = words[Math.floor(Math.random()*words.length)];
+        front = item.word;
+        back1 = item.roman;
+        back2 = item.meaning;
+        break;
+      case 'sentence':
+        item = sentences[Math.floor(Math.random()*sentences.length)];
+        front = item.sentences;
+        back1 = item.roman;
+        back2 = item.meaning;
+        break;
+      default:
+        jpFn.selectedCat = $('select#cat option:selected').val();
+        if(jpFn.selectedCat === 'random'){
+          jpFn.selectedCat = $($('select#cat option')[ Math.floor(Math.random() * ($('select#cat option').length-1) + 1)]).val();
+        }
+        $('select#cat option').each(function(a,b){
+          var optionValue = $(b).val();
+          if(optionValue != 'random' && optionValue != jpFn.selectedCat){
+                jpFn.unselectedCat.push(optionValue);       
+          }
+        })
+        item = gojuons[Math.floor(Math.random()*gojuons.length)];
+        front = item[jpFn.selectedCat];
+        back1 = item[jpFn.unselectedCat[0]];
+        back2 = item[jpFn.unselectedCat[1]];
+        $('.flip .card p#second').show();
+        break;
+    }
+    $('.flip').addClass(jpFn.selectedType);
+    jpFn.previousType = jpFn.selectedType;
+    setTimeout(function(){
+      $('.card.front p').html(front);
+      $('.card.back p#first').html(back1);
+      $('.card.back p#second').html(back2);
+      jpFn.unselectedCat = [];
+    }, 200)
+
+  },
+  assignClick: function(){
+    $(document).on('click', '#generate', function(){
+      jpFn.randomPick();
+    });
+    $('#type').on('change', function(){
+      jpFn.selectedType = $(this).val();
+      if(jpFn.selectedType == 'gojuon'){
+        $('#cat').show()
+      }
+      else{
+        $('#cat').hide()
+      }
+      jpFn.randomPick();
+    })
+  }
+}
+$(document).ready(function(){
+  jpFn.init();
+})
